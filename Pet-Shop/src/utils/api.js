554dc82566ebbb47e14f342242883d0b4e6a8774
Denis Arefyev <163993361+DenisArefyev@users.noreply.github.com>
2024-08-07 11:48:53 +0200

    added vercel

--- Pet-Shop/src/utils/api.js
+++ Pet-Shop/src/utils/api.js
@@ -1,5 +1,5 @@
-// const API_URL = "http://localhost:3333";
+const API_URL = "http://localhost:3333";
 
-const API_URL = "https://pet-shop-backend.slavab.kz";
+// const API_URL = "https://pet-shop-backend.slavab.kz";
 
 export default API_URL;
